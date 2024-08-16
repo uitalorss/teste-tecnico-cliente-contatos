b5c483c8c0a19465ce84b72b3be58f88765773f4
Uítalo Souza <uitalo.urss@gmail.com>
2024-08-16 14:49:39 -0300

    create contact module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { DatabaseModule } from "./database/database.module";
 import { UserModule } from "./user/user.module";
 import { ContactService } from './contact/contact.service';
+import { ContactModule } from './contact/contact.module';
 
 @Module({
-  imports: [DatabaseModule, UserModule],
+  imports: [DatabaseModule, UserModule, ContactModule],
   controllers: [],
   providers: [ContactService],
 })
