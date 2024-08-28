1b2f2cca705007e3222d07affe33d62aad2db948
Uítalo Souza <uitalo.urss@gmail.com>
2024-08-28 17:56:08 -0300

    [BACKEND] fix bug in schema to allow multiple emails and phones

--- backend/src/user/dto/create-user.dto.ts
+++ backend/src/user/dto/create-user.dto.ts
@@ -8,7 +8,7 @@ export const createUserSchema = z.object({
     .trim()
     .min(5, "Campo precisa ter mais de 5 caracteres")
     .max(15, "campo precisa ter menos de 15 caracteres"),
-  emails: z.array(z.string().email({ message: "Email inválido" })).nonempty("Campo não pode ficar vazio"),
+  emails: z.array(z.string().trim().email({ message: "Email inválido" })).nonempty("Campo não pode ficar vazio"),
   phones: z.array(z.string().regex(/^\(\d{2}\)\s?\d{4,5}-?\d{4}$/, { message: "Favor informar números de telefones válidos" })).nonempty("Campo não pode ficar vazio"),
 });
 
